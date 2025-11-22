1854f421475d0c16b53923334b8d943a50c0e6a6
地球が動き出した <79787986+moveofearth@users.noreply.github.com>
2025-11-22 19:49:41 +0900

    파티클 시스템 수정

--- ParticleSystem.h
+++ ParticleSystem.h
@@ -2,42 +2,27 @@
 #include "CommonInclude.h"
 #include "Particle.h"
 #include <vector>
+#include "Mapping/shader.hpp"
 
-class ParticleSystem
-{
+class ParticleSystem {
 private:
 	std::vector<Particle> particles;
-	GLuint maxParticles;
-	GLfloat emissionRate;
-	GLfloat timeSinceLastEmission;
-	
-	// OpenGL 버퍼
-	GLuint VAO, VBO;
-	std::vector<GLfloat> vertexData;
+	size_t maxParticles;
+
+	GLuint cubeVAO=0, cubeVBO=0, cubeEBO=0;
+	GLuint instanceVBO=0;
+	GLuint particleShaderProgram=0;
+	std::vector<glm::vec4> instanceData; // xyz position, w scale
 
 public:
-	ParticleSystem(GLuint maxParticleCount = 1000);
-	~ParticleSystem();
-	
-	// 파티클 방출 (미사일 위치에서)
-	void emitParticle(const glm::vec3& position, const glm::vec3& baseVelocity);
-	
-	// 연기 효과 업데이트
-	void update(GLfloat deltaTime);
-	
-	// 파티클 렌더링
-	void render();
-	
-	// 방출률 설정
-	void setEmissionRate(GLfloat rate) { emissionRate = rate; }
-	
-	// 모든 파티클 제거
+	ParticleSystem(size_t maxCount=1000);
+	void initialize();
+	void emitParticle(const glm::vec3& pos, const glm::vec3& vel);
+	void update(float dt);
+	void render(const glm::mat4& view, const glm::mat4& proj);
 	void clear();
-	
-	// 활성 파티클 수 반환
-	int getActiveParticleCount() const;
-	
+
 private:
-	void setupBuffers();
-	void updateVertexData();
+	void setupCubeGeometry();
+	void updateInstanceBuffer();
 };
